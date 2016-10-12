6056b1361a2907d17e43c365ca8ea5e27b47d295
Alexander-Andrade <veimar.94@gmail.com>
2016-10-12 23:14:38 +0300

    full scheme test

--- apwmis_2.srcs/sim_1/new/full_scheme_test.vhd
+++ apwmis_2.srcs/sim_1/new/full_scheme_test.vhd
@@ -38,7 +38,7 @@ begin
        not_sload<='1';
        not_aclr<='1';
        not_aload<='1';
-       wait for 100 ns;
+       wait for 95 ns;
    --async clear
        not_oe<='0';
        not_aclr<='0';
@@ -49,11 +49,46 @@ begin
        c<='0';
        d<='1'; 
    
+       not_oe<='0';
        not_aclr<='1';
        not_aload<='0';
        wait for 100 ns; 
    --sync clear
-       
+       not_oe<='0';
+       not_sclr<='1';
+       not_aload<='1';
+       not_sclr<='0';
+       wait for 100 ns;
+    --sync load
+       a<='1';
+       b<='1';
+       c<='0';
+       d<='0'; 
+    
+       not_oe<='0';
+       not_aclr<='1';
+       not_aload<='1';
+       not_sclr<='1';
+       not_sload<='0';
+       wait for 100 ns;
+     --counting
+      not_oe<='0';
+      ent<='1';
+      enp<='1';
+      not_sclr<='1';
+      not_sload<='1';
+      not_aclr<='1';
+      not_aload<='1';
+      wait for 100 ns;
+     --inhibit counting
+      not_oe<='0';
+      ent<='0';
+      enp<='1';
+      not_sclr<='1';
+      not_sload<='1';
+      not_aclr<='1';
+      not_aload<='1';
+      wait for 100 ns;
    end process;
          
 end Behavioral;
